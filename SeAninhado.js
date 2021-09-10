1bdfd96e6249738920f2bb4149eb1136bbd0c90f
Matheus Polidoro <matheus.polidoro@senaisp.edu.br>
2021-09-10 10:27:30 -0300

    atualização

---
+++ SeAninhado.js
@@ -0,0 +1,8 @@
+var login = "fulano";
+var senha = "a1234";
+if (login == "fulano") 
+    console.log ("digite a senha");
+else if (senha == "a1234") 
+    console.log ("bem-vindo");
+else
+    console.log ("login ou senha incorretos");
---
+++ SeAninhado.py
@@ -0,0 +1,8 @@
+login = str(input('Digite o login: '))
+senha = '123'
+if login == '123':
+    senha = str(input('Digite o senha: '))
+elif senha == '123':
+    print('Bem vindo')
+else:
+    print('Senha ou Login incorretos!')
